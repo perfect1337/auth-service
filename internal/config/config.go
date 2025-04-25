75e1d24d731f8748045eede6eac680ee4ca1d3b6
perfect1337 <bryunine2005@gmail.com>
2025-04-25 22:25:16 +0400

    фул

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,16 +1,10 @@
 package config
 
 import (
-	"os"
 	"time"
-
-	"github.com/joho/godotenv"
 )
 
 type Config struct {
-	Server struct {
-		Port string
-	}
 	Postgres struct {
 		Host     string
 		Port     string
@@ -19,54 +13,37 @@ type Config struct {
 		DBName   string
 		SSLMode  string
 	}
+	Server struct {
+		Port string
+	}
 	Auth struct {
-		SecretKey            string
 		AccessTokenDuration  time.Duration
 		RefreshTokenDuration time.Duration
+		SecretKey            string
 	}
-	GRPC struct {
-		Port string
+	Migrations struct {
+		Enable bool
 	}
 }
 
 func Load() *Config {
-	_ = godotenv.Load()
-
 	cfg := &Config{}
 
-	// Server
-	cfg.Server.Port = getEnv("SERVER_PORT", "8081")
-
 	// Postgres
-	cfg.Postgres.Host = getEnv("DB_HOST", "localhost")
-	cfg.Postgres.Port = getEnv("DB_PORT", "5432")
-	cfg.Postgres.User = getEnv("DB_USER", "postgres")
-	cfg.Postgres.Password = getEnv("DB_PASSWORD", "postgres")
-	cfg.Postgres.DBName = getEnv("DB_NAME", "auth_service")
-	cfg.Postgres.SSLMode = getEnv("DB_SSLMODE", "disable")
+	cfg.Postgres.Host = "localhost"
+	cfg.Postgres.Port = "5432"
+	cfg.Postgres.User = "postgres"
+	cfg.Postgres.Password = "postgres"
+	cfg.Postgres.DBName = "PG"
+	cfg.Postgres.SSLMode = "disable"
+
+	// Server
+	cfg.Server.Port = "8080"
 
 	// Auth
-	cfg.Auth.SecretKey = getEnv("AUTH_SECRET_KEY", "secret-key")
-	cfg.Auth.AccessTokenDuration = getEnvAsDuration("ACCESS_TOKEN_DURATION", 15*time.Minute)
-	cfg.Auth.RefreshTokenDuration = getEnvAsDuration("REFRESH_TOKEN_DURATION", 24*time.Hour)
-
-	// GRPC
-	cfg.GRPC.Port = getEnv("GRPC_PORT", "50051")
-
+	cfg.Auth.AccessTokenDuration = 15 * time.Minute
+	cfg.Auth.RefreshTokenDuration = 360 * time.Hour
+	cfg.Auth.SecretKey = "your-secret-key"
+	cfg.Migrations.Enable = false
 	return cfg
-}
-
-func getEnv(key, defaultValue string) string {
-	if value, exists := os.LookupEnv(key); exists {
-		return value
-	}
-	return defaultValue
-}
-
-func getEnvAsDuration(name string, defaultVal time.Duration) time.Duration {
-	valueStr := getEnv(name, "")
-	if value, err := time.ParseDuration(valueStr); err == nil {
-		return value
-	}
-	return defaultVal
 }
