066dab04eb6a46368c73dc1c610ef468bba185d3
perfect1337 <bryunine2005@gmail.com>
2025-04-28 19:49:47 +0400

    уд ком

--- migrations/004_create_chat_messages.up.sql
+++ migrations/004_create_chat_messages.up.sql
@@ -1,10 +1,10 @@
 
 CREATE TABLE chat_messages (
-    id SERIAL PRIMARY KEY,
-    user_id INTEGER NOT NULL REFERENCES users(id),
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    user_id INT REFERENCES users(id),
     author VARCHAR(255) NOT NULL,
     text TEXT NOT NULL,
-    created_at TIMESTAMP NOT NULL DEFAULT NOW()
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE INDEX idx_chat_messages_created_at ON chat_messages(created_at);
